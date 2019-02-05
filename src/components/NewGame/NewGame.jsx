f549f5f8521b2946dd50cbbb614bde663c845d3e
Kyle Knight <kyleaknight@gmail.com>
2019-02-05 12:35:43 -0600

    Updated icons and logos

--- src/components/NewGame/NewGame.jsx
+++ src/components/NewGame/NewGame.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import logo from './logo.svg';
+
 class NewGame extends React.Component {
   state = {
     playerCount: 1,
@@ -38,52 +40,119 @@ class NewGame extends React.Component {
 
     return (
       <Modal>
-        <h1>Start New Game</h1>
-        <ol>
-          <li>
-            <h2>Choose Starting Life</h2>
-            <input
+        <img src={logo} alt="Spindown - a counter app" />
+
+        <SetupSteps>
+          <StepItem>
+            <StepName>Choose Starting Life</StepName>
+            <StartingLifeInput
               type="number"
               value={startingLife}
               onChange={this.updateStartingLife}
             />
-          </li>
+          </StepItem>
 
-          <li>
-            <h2>Choose Number of Players</h2>
-            <select onChange={this.updatePlayerCount}>
+          <StepItem>
+            <StepName>Choose Number of Players</StepName>
+            <PlayerCount onChange={this.updatePlayerCount}>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
-            </select>
-          </li>
+            </PlayerCount>
+          </StepItem>
 
-          <li>
+          <StepItem>
             <Button onClick={this.newGame}>Start Game</Button>
-          </li>
-        </ol>
+          </StepItem>
+        </SetupSteps>
       </Modal>
     );
   }
 }
 
 const Modal = styled.div`
-  width: 80vw;
+  box-sizing: border-box;
+  width: 100vw;
   position: fixed;
-  height: 80vh;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: ${props => props.theme.colors.base.light};
-  border: 1px solid ${props => props.theme.colors.tertiary.a};
-  border-radius: 4px;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #0a0a0a;
   z-index: 10;
+  display: flex;
+  flex-direction: column;
+  padding: 2em 0;
+`;
+
+const SetupSteps = styled.ol`
+  box-sizing: border-box;
+  list-style: none;
+  margin: 2em 2em 0;
+  padding: 2em 2em 0;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  border-top: 1px solid ${props => props.theme.colors.primary.b};
+`;
+
+const StepItem = styled.li`
+  margin: 0 0 0.15em;
+  display: flex;
+  flex-direction: column;
+
+  &:last-child {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0 0 0.5em;
+  }
+`;
+
+const StepName = styled.h2`
+  font-weight: 400;
+  font-size: 1.5em;
+  margin: 0.25em 0;
+  color: ${props => props.theme.colors.secondary.c};
 `;
 
 const Button = styled.button`
   border: 1px solid #000;
+  font-size: 1.5em;
+  width: 100%;
+  border-radius: 3px;
+  background-color: ${props => props.theme.colors.primary.a};
+  color: ${props => props.theme.colors.secondary.c};
+  padding: 0.25em 0;
+  box-shadow: 1px 1px 3px ${props => props.theme.colors.tertiary.c};
+`;
+
+const StartingLifeInput = styled.input`
+  border-radius: 3px;
+  background-color: #0a0a0a;
+  border: 1px solid ${props => props.theme.colors.tertiary.c};
+  color: ${props => props.theme.colors.primary.c};
   font-size: 18px;
+`;
+
+const PlayerCount = styled.select`
+  border-radius: 3px;
+  background-color: #0a0a0a;
+  border: 1px solid ${props => props.theme.colors.tertiary.c};
+  color: ${props => props.theme.colors.primary.c};
+  font-size: 18px;
+  position: relative;
+
+  :after {
+    content: 'OPEN';
+    color: red;
+    position: absolute;
+    right: 0;
+    top: 50%;
+  }
 `;
 
 export default NewGame;
